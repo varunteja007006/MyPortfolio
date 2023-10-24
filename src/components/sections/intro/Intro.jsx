9665825723ac99a221fdc9e5a831bd711f403670
Varun <varunteja007006@gmail.com>
2023-10-24 20:05:59 +0530

    Updated Intro

--- src/components/sections/intro/Intro.jsx
+++ src/components/sections/intro/Intro.jsx
@@ -1,53 +1,62 @@
 import { motion } from "framer-motion";
 import profile from "../../../assets/profile.webp";
-import resume from "../../../assets/varun_resume.pdf";
+import resume from "../../../assets/VarunK_Resume_FS.pdf";
 import { BsDownload } from "react-icons/bs";
 import Typewriter from "typewriter-effect";
 
 function Intro() {
   return (
     <>
-      <div className="hero min-h-screen ">
-        <div className="hero-content flex-col lg:flex-row lg:items-start">
+      <div className="hero min-h-screen pt-5 p-10 ">
+        <div className="hero-content flex-col lg:flex-row lg:items-start p-0">
           {/* profile pic */}
-          <span className="flex flex-col gap-5 text-white text-center">
-            <img src={profile} className="max-w-sm rounded-lg shadow-2xl" />
-            <span className="text-xl text-black dark:text-white">
+          <div className="flex flex-col  text-white text-center items-center lg:w-1/3">
+            <img
+              src={profile}
+              className="rounded-lg shadow-2xl  w-full h-full"
+            />
+            <span className="text-xl text-black dark:text-white mt-5 w-full h-10">
               <Typewriter
                 options={{
-                  strings: ["Full Stack Developer", "Python Developer"],
+                  strings: [
+                    "Full Stack Developer | React JS | Node JS | JavaScript",
+                    "Python Developer | Python | Django",
+                  ],
                   autoStart: true,
                   loop: true,
                 }}
               />
             </span>
-          </span>
-          <div>
+          </div>
+          <div className="lg:w-2/3">
             {/* intro content */}
             <div className="md:px-10 lg:px-14 text-justify font-normal grid justify-items-stretch cursor-default">
               <p className="pb-3 ">
                 I am a passionate developer who loves to work with different
                 tech stacks, both front-end and back-end. I have hands-on
-                experience with various languages and frameworks, such as
-                Django, Python, HTML, CSS (Bootstrap & Tailwind), JavaScript,
-                React JS, Node JS, Mongo DB, MySQL, PHP, and also tools like
-                GIT, Tortoise SVN, and Jenkins.
+                experience with various programming languages and frameworks,
+                such as JavaScript, React JS, Node JS, MongoDB, HTML, CSS
+                (Bootstrap & Tailwind), MERN stack, Django, Python, MySQL, PHP,
+                AND tools like GIT, Tortoise SVN, SonarQube and Jenkins.
               </p>
               <p className="pb-3 ">
-                My primary focus is on developing web apps, apps, and Python
-                scripting for automation, but I am always eager to explore new
+                Currently, I work as System Engineer for SuccessFactors HXM
+                platform development, developing custom responsive interfaces
+                tailored to meet client requirements. I am keen to expand my
+                domain in other fields of technology. Therefore I am open to
+                opportunities.
+                {/* Therefore, I am open to exploring new
+                opportunities and collaborating with individuals and
+                organizations on exciting projects.  */}
+              </p>
+              <p className="pb-3 ">
+                I love developing web apps and python scripting for automation &
+                day-to-day tasks that can be made easy with python. I am always
+                eager to explore new technologies and I keep myself relevant
                 technologies.
                 {/* I am particularly interested in cutting-edge
                 technologies such as cloud computing, web3, blockchain, and
                 smart contracts. */}
-              </p>
-              <p className="pb-3 ">
-                Currently, I work as System Engineer for SuccessFactors HXM
-                platform development, but I am keen to expand my domain in other
-                fields of technology. Therefore I am open to opportunities.
-                {/* Therefore, I am open to exploring new
-                opportunities and collaborating with individuals and
-                organizations on exciting projects.  */}
               </p>
               {/* <p className="pb-3 ">
                 Thank you for visiting my portfolio website, and I hope you
