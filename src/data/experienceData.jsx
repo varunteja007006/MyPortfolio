c54884180b7f7bf4ebc7daaeb9ca58b2054dcaae
varun <varunteja007006@gmail.com>
2023-12-06 11:25:43 +0530

    updated Repo

--- src/data/experienceData.jsx
+++ src/data/experienceData.jsx
@@ -7,7 +7,7 @@ const experienceData = [
     workingTill: "Dec 2023",
     dateOfJoining: "2021-5-27",
     jobDescription: [
-      "Profession: System Engineer - SuccessFactors HXM Platform Development.",
+      "Profession: System Engineer - HCM Platform Development.",
       "Responsibilities: Developing and supporting custom interfaces, specifically tailored to meet client requirements for a global user base of 10,000+ across four critical regions: America, APAC, EMEA, and JAPAN.",
       "Additional Info: Built custom interfaces using Recharts, React Hooks, Fetch API, supporting  JWT Authentication.",
       "Success milestone: Received a 5-star rating from customers for consistently providing timely updates and exceptional support.",
